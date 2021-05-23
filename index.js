69a64d33e873c46a389f02948c2a6e52bfa40133
Masudha Meher <>
2021-05-23 12:25:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+var readlineSync = require("readline-sync");
+
+var i = 0, arr = [];
+//1. Welcoming the user
+function welcome() {
+  var username = readlineSync.question("Can I have your name? ");
+  console.log("Hi " + username + "!");
+}
+
+//2. List of commands
+function todoCommands() {
+  console.log("Here's a list of things you can do here:");
+  console.log("1. Add your todo to the list");
+  console.log("2. Show all your todos");
+  console.log("3. Delete a Todo from your list");
+  console.log("4. Edit your todo from the list");
+
+  var optionNumber = readlineSync.question("Type the option number: ");
+  switch (optionNumber) {
+    case '1' || '1.':
+      add();
+      break;
+    case '2' || '2.':
+      show();
+      break;
+    case '3'||'3.':
+    var arrNew = [];
+    arrNew=deletee();
+    console.log(arrNew);
+      break;
+    default:
+    console.log("Undefined option");
+    break;
+  }
+}
+
+//3. Adding a items to the list
+function add() {
+  var itemNumber = readlineSync.question("Type in your pending work! How many items do you want add? ");
+  for (i = 0; i < itemNumber; i++) {
+    var item = readlineSync.question("Inserting: ");
+    if(item == ""){
+      console.log("Empty value can't be inserted.")
+    }
+    arr.push(item);
+  }
+  console.log("-----------------");
+  console.log("Added items successfully!");
+  show();
+  todoCommands();
+}
+
+//4. Show all the items
+function show() {
+  if(arr.length == 0){
+    console.log("Oops, your list is empty. Please add some items.");
+    console.log("----------------------");
+    todoCommands();
+  }
+  console.log("Your todo list is as follows:\n");
+  console.log("----------------");
+  for(i = 0; i<arr.length; i++ ){
+    console.log(i + "." + arr[i] + "\n");
+  }
+  console.log("----------------");
+}
+
+//5. Delete an item
+function deletee() {
+  show();
+  if(arr.length == 0){
+    console.log("Oops, your list is empty. Please add some items.");
+    console.log("----------------------");
+    todoCommands();
+  }
+  var itemIndex = readlineSync.question("Specify the array index: ");
+  console.log("----------------");
+  arr.splice(itemIndex, 1);
+  show();
+}
+
+welcome();
+todoCommands();
